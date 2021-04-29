6605a7caa785a00334dabc2123772d39ca5bd64d
Edu <61661507+edu-bio@users.noreply.github.com>
2021-04-30 01:36:03 +0200

    --- Auto-Git Commit ---

--- src/suma.test.ts
+++
@@ -1,9 +0,0 @@
-import { sum } from './suma';
-
-  test('suma sin argumentos', () => {
-    expect(sum()).toBe(0);
-  });
-
-  test('suma con dos arguemntos', () => {
-    expect(sum(1, 2)).toBe(3);
-  });
