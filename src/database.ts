18fcc542aff6483d256b3bf421499a9b6ef1e031
Edu <61661507+edu-bio@users.noreply.github.com>
2021-05-07 09:50:41 +0200

    --- Auto-Git Commit ---

--- src/database.ts
+++ src/database.ts
@@ -2,7 +2,8 @@ import { createPool } from 'mysql2/promise';
 
 export async function conectar() {
     const connection = await createPool({
-        host: '192.168.1.37',
+        //host: '192.168.1.37',
+        host: '192.168.1.14',
         port: 33060,
         user: 'root',
         password: 'root',
